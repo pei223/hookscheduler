85295fa082d9d4de24dd0a06162d17150cf127da
pei223 <peidparc@gmail.com>
2025-02-04 20:27:46 +0900

    :adhesive_bandage: トランザクション周りのモジュール修正

--- pkg/db/tx_scope.go
+++ pkg/db/tx_scope.go
@@ -4,40 +4,54 @@ import (
 	"context"
 	"database/sql"
 
-	"github.com/rs/zerolog"
+	"github.com/pei223/hook-scheduler/pkg/logger"
 )
+
+func ReadOnlyTx(
+	ctx context.Context,
+	db *sql.DB,
+	f func(ctx context.Context, tx *sql.Tx) error,
+	option *sql.TxOptions,
+) error {
+	newOption := &sql.TxOptions{
+		ReadOnly: true,
+	}
+	if option != nil {
+		newOption.Isolation = option.Isolation
+	}
+	return ExecTx(ctx, db, f, newOption)
+}
 
 func ExecTx(
 	ctx context.Context,
-	logger *zerolog.Logger,
 	db *sql.DB,
 	f func(ctx context.Context, tx *sql.Tx) error,
-	options ...sql.TxOptions,
+	option *sql.TxOptions,
 ) error {
-	option := &sql.TxOptions{}
-	for _, opt := range options {
-		option = &opt
-	}
-
+	logger := logger.FromContext(ctx)
+	var err error
 	tx, err := db.BeginTx(ctx, option)
 	if err != nil {
-		logger.Error().Err(err).Msg("failed to begin transaction")
+		logger.Warn().Err(err).Msg("failed to begin transaction")
 		return err
 	}
 	defer func() {
-		err := tx.Rollback()
 		if err != nil {
-			logger.Error().Err(err).Msg("failed to rollback")
+			logger.Warn().Err(err).Msg("transaction failed. try rollback")
+			err := tx.Rollback()
+			if err != nil {
+				logger.Error().Err(err).Msg("failed to rollback")
+			}
 		}
 	}()
 
-	if err := f(ctx, tx); err != nil {
+	if err = f(ctx, tx); err != nil {
 		return err
 	}
 
 	err = tx.Commit()
 	if err != nil {
-		logger.Error().Err(err).Msg("failed to commit")
+		logger.Warn().Err(err).Msg("failed to commit")
 	}
 	return err
 }
